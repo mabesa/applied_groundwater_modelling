6d3951f09041f1bd6187af89f9f881415b6f3265
LouiseNoelDP <lnoeldu@student.ethz.ch>
2025-04-01 15:57:54 +0200

    Work tuesday afternoon : figures display, exercise03 darcy law implementation, combine tasks 02 - '3 of exercise 1.

--- exercises_complements/tasks_data.py
+++ exercises_complements/tasks_data.py
@@ -15,15 +15,30 @@ We can assume the system to be at a steady state.
 """,
 
 "task01_2":  r"""
-## Task 2:
+## Task additionnal 1:
 The mean residence time of water $\tau$ was estimated to be 1 year.  
 - **Estimate the total volume of the aquifer $V_{aq}$**
 """,
 
 "task01_3": r"""
-## Task 3:
+## Task 2:
 Based on the previous results :
 - **Estimate $M_{aq}$ the mean thickness of the aquifer**" 
+""",
+
+"task03_1": r"""
+## Task 1:
+Based on given data, estimate the specific discharge $q$ : 
+""",
+
+"task03_2": r"""
+## Task 2:
+Based on given data, estimate the  discharge $Q$ in the soil column :
+""",
+
+"task03_3": r"""
+## Task 3:
+Based on given data, estimate the mean velocity of water $u$ in the soil column :
 """,
 
 "task04_1": r"""
@@ -43,7 +58,10 @@ Based on given data, estimate the water table level at $x$ = 400m :
 solutions = {
     "task01_1": (1.6, 1.8),  # Correct solution 1.7
     "task01_2": (3100000, 3200000),  # Correct solution 3153600
-    "task01_3": (1.8, 2.0),  # Correct solution 1.9
+    "task01_3": (0.18, 0.20),  # Correct solution 1.9
+    "task03_1": (1.1, 1.3), 
+    "task03_2": (0.60, 0.62), 
+    "task03_3": (4.7, 4.9), 
     "task04_1": (37, 39),  # Correct solution 38
     "task04_2": (25, 27),  # Correct solution 26
     # Add more tasks and their correct intervals here
@@ -55,7 +73,10 @@ solutions = {
 solutions_exact = {
     "task01_1": "1.7 ",
     "task01_2": "3153600 ",
-    "task01_3": "1.9 ",
+    "task01_3": "0.19 ",
+    "task03_1": "1.2",
+    "task03_2": "0.61",
+    "task03_3": "4.8",
     "task04_1": "38.0",  
     "task04_2": "26.0"
     # Add more tasks and their correct intervals here
@@ -72,6 +93,9 @@ solution_unit = {
     "task01_1": "km^2",
     "task01_2": "m^3",
     "task01_3": "m",
+    "task03_1": "mm/s",
+    "task03_2": "cm^3/s",
+    "task03_3": "mm/s",
     "task04_1": "m",  
     "task04_2": "m"
     # Add more tasks and their correct intervals here
@@ -99,9 +123,8 @@ So we have that $ P \times A \times t = Q \times t $
 
 We can solve $ A = \frac{1728 \times 10^{-9}}{1 \times 10^{-6}} \simeq 1.7 \text{km}^2 $
 
-This result is close to the value from the BAFU :
 
-<img src="sources/SwissTopoTsaletArea.png" alt="Spring Catchment Area" style="width:50%;">""",
+""",
 
 "task01_2": r"""
 
@@ -111,13 +134,46 @@ Overall we have that $V_{aq} = \frac{V_{out}}{\phi}$ where :
 - $V_{out} = Q \times \tau = 1728 \text{m}^3/\text{day} \times 365 \text{ day} = 1728 \times 365 \text{m}^3 $
 - ${\phi} = 0.2$
 
-Finally, $V_{acq} = 1728 \times 365 \text{m}^3 / 0.2 = 3153600 \text{m}^3$
+Finally, $V_{aq} = 1728 \times 365 \text{m}^3 / 0.2 = 3153600 \text{m}^3$
 """,
 
 
 "task01_3": r"""
 
-The thickness is : $M_{aq} = \frac{V_{acq}}{A} = \frac{3123600}{1.7} \frac{ \text{m}^3 }{\text{km}^2} = \frac{3123600}{1.7 \times 10^6}\text{m} \simeq 1.9 \text{m}$
+The thickness is : $M_{aq} = \frac{V_{aq}}{A}$. $A$ being known, we still need to compute the aquifer volume $V_{aq}$.
+
+The volume of water $V_{out}$ going out from the aquifer in one residence time period $\tau$ is needed to estimate the volume of the aquifer itself $V_{aq}$. We also need to take into account the aquifer porosity. 
+
+Overall we have that $V_{aq} = \frac{V_{out}}{\phi}$ where :
+- $V_{out} = Q \times \tau = 1728 \text{m}^3/\text{day} \times 365 \text{ day} = 1728 \times 365 \text{m}^3 $
+- ${\phi} = 0.2$
+
+Finally, $V_{aq} = 1728 \times 365 \text{m}^3 / 0.2 = 3153600 \text{m}^3$
+
+Back to the mean aquifer thickness : $M_{aq} = \frac{V_{aq}}{A} = \frac{3123600}{1.7} \frac{ \text{m}^3 }{\text{km}^2} = \frac{3123600}{1.7 \times 10^6}\text{m} \simeq 1.9 \text{m}$
+""",
+
+"task03_1":
+r"""
+According to Darcy 's law, we have that :
+
+$q = KI = K \cdot \frac{\Delta h}{L} = 0.0003 \cdot \frac{0.4}{1} = 1.2 \cdot 10^{-4} \cdot\text{m}/\text{s}^{-1} = 0.12 \cdot \text{cm}/\text{s}^{-1}$.
+""",
+
+"task03_2":
+r"""
+The discharge is derived from the specific discharge, accounting for the soil column cross section area : 
+
+$Q = q \cdot A = 
+1.2 \cdot 10^{-4} \cdot \pi \cdot \frac{d}{2}^2 = 6.1 \cdot 10^{-8} \cdot\text{mm}^3/\text{s}^{-1} = 6.1 \cdot \text{cm}^3/\text{s}^{-1}$.
+""",
+
+"task03_3":
+r"""
+The mean velocity is derived from the specific discharge, accounting for the soil column effective porosity : 
+
+$u = \frac{q}{\phi_e} = 
+ \frac{1.2 \cdot 10^{-4}}{0.25} = 4.8 \cdot 10^{-4} \cdot\text{m}/\text{s}^{-1} = 0.48 \cdot \text{cm}/\text{s}^{-1}$.
 """,
 
 "task04_1": r"""
